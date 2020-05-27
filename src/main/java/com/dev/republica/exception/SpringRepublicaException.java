57376a15966ecec2c26be99524c85670b83f984e
Camatta <felipecamattabergi@gmail.com>
2020-05-27 18:08:27 -0300

    Login

---
+++ src/main/java/com/dev/republica/exception/SpringRepublicaException.java
@@ -0,0 +1,7 @@
+package com.dev.republica.exception;
+
+public class SpringRepublicaException extends RuntimeException {
+    public SpringRepublicaException(String exMessage) {
+        super(exMessage);
+    }
+}
---
+++ src/main/java/com/dev/republica/model/User.java
@@ -0,0 +1,45 @@
+package com.dev.republica.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import java.time.Instant;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Username is required")
+    private String username;
+
+    @NotBlank(message = "Password is required")
+    private String password;
+
+    @Email
+    @NotEmpty(message = "Email is required")
+    private String email;
+
+    private Instant created;
+
+    private boolean enabled;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "role_user", joinColumns = {
+            @JoinColumn(name = "user_id", referencedColumnName = "id")}, inverseJoinColumns = {
+            @JoinColumn(name = "role_id", referencedColumnName = "id")})
+    private List<Role> roles;
+
+}
