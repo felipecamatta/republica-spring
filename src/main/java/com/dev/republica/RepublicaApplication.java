782710d967875dd7c9b166a1f224c7a4951a8ed1
Camatta <felipecamattabergi@gmail.com>
2020-05-27 14:07:37 -0300

    First commit

---
+++ src/main/java/com/dev/republica/RepublicaApplication.java
@@ -0,0 +1,13 @@
+package com.dev.republica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepublicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepublicaApplication.class, args);
+	}
+
+}
