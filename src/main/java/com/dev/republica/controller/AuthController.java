31cb2fb6804ad2c55520e224e80079f70b228179
Camatta <felipecamattabergi@gmail.com>
2020-05-29 02:19:42 -0300

    Removido importações desnecessárias e realizado code format

--- src/main/java/com/dev/republica/controller/AuthController.java
+++ src/main/java/com/dev/republica/controller/AuthController.java
@@ -8,7 +8,10 @@ import com.dev.republica.service.AuthService;
 import com.dev.republica.service.RefreshTokenService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
